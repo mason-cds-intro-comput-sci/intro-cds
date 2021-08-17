4d09e2921be4e9143944665f4df1c3068b0fa0dc
Dominic White (Travis-CI) <dwhite34@gmu.edu>
2021-08-17 16:26:36 -0400

    Add local deploy script

--- _deploy_from_local.sh
+++ _deploy_from_local.sh
@@ -2,22 +2,25 @@
 
 set -e
 
+#[ -z "${GITHUB_PAT}" ] && exit 0
+#[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
 # Set git config information
 git config --global user.name "Dominic White (Travis-CI)"
 git config --global user.email "dwhite34@gmu.edu"
 
 # Clone the gh-pages repository
 git clone -b gh-pages \
-    https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+    https://github.com/mason-cds-intro-comput-sci/intro-cds.git \
     book-output
 
 # Change to the gh-page clone book-output directory
 cd book-output
 
 # Copy generated output to book-output
-cp -r ../_book/* ./
+cp -r ../docs/* ./
 
 # Add all files to the repo
 git add *
-git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
+git commit -a -m "Updating book "
 git push -q origin gh-pages
